e7e41ac9ab0328d4f0225cb403c9196e00e8468d
Alvy <alvyahmed03@gmail.com>
2025-04-25 22:30:35 +0600

    All the basic functionalities added

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -6,10 +6,11 @@ import { getRandomInterviewCover } from '@/lib/utils';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
+import { getFeedbackByInterviewId } from '@/lib/actions/general.action';
 
-const InterviewCard = ({ id, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
+const InterviewCard = async ({ id, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
 
-    const feedback = null as Feedback | null;
+    const feedback = (userId && id)? await getFeedbackByInterviewId({interviewId: id, userId});
     const normalizedType = /mix/i.test(type)? 'Mix': type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
 
